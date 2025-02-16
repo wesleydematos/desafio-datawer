914e206a93c80b2f0fa9aac47bd7bb255f1604de
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2025-02-16 20:34:20 -0300

    feat: update scrollbar colors

--- src/styles/ThemeRegistry.tsx
+++ src/styles/ThemeRegistry.tsx
@@ -3,6 +3,7 @@
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "@/styles/theme";
 import CssBaseline from "@mui/material/CssBaseline";
+import { GlobalStyles } from "@mui/system";
 
 export default function ThemeRegistry({
   children,
@@ -12,6 +13,24 @@ export default function ThemeRegistry({
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <GlobalStyles
+        styles={{
+          "::-webkit-scrollbar": {
+            width: 8,
+          },
+          "::-webkit-scrollbar-track": {
+            backgroundColor: "#f0f0f0",
+            borderRadius: 4,
+          },
+          "::-webkit-scrollbar-thumb": {
+            backgroundColor: "#48c1d0",
+            borderRadius: 4,
+            "&:hover": {
+              backgroundColor: "#02a5bc",
+            },
+          },
+        }}
+      />
       {children}
     </ThemeProvider>
   );
